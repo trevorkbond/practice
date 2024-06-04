12f76fbe4108426709e63afb33a9704821832996
Trevor Bond <trevorkbond@gmail.com>
2024-06-04 11:33:42 -0600

    Add set functions to implement

---
+++ DSA/BinaryTree/src/main/BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree {
+    private Node root;
+    private int size;
+
+    public boolean add(int insertVal) {
+        return false;
+    }
+
+    public boolean remove(int removeVal) {
+        return false;
+    }
+
+    public boolean contains(int findVal) {
+        return false;
+    }
+}
---
+++ DSA/BinaryTree/src/main/Node.java
@@ -0,0 +1,29 @@
+public class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
